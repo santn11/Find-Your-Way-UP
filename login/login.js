cf7e7cb9433d8208f1a368ab00e3dfb95efdba28
santn11 <fernandonogueira.nogueira768@gmail.com>
2023-10-24 21:56:53 -0300

    second commit

--- login/login.js
+++ login/login.js
@@ -1,9 +1,30 @@
 document.querySelector('.cad').onclick = () => {
     document.querySelector('#login').style.display = 'none'
     document.querySelector('#cad').style.display   = 'flex'  
+    document.querySelector('#nova-senha').style.display = 'none'
 }
 
 document.querySelector('.log').onclick = () => {
     document.querySelector('#cad').style.display    = 'none'  
     document.querySelector('#login').style.display  = 'flex' 
+    document.querySelector('#nova-senha').style.display = 'none'
 }
+
+document.querySelector('.log').onclick = () => {
+    document.querySelector('#cad').style.display    = 'none'  
+    document.querySelector('#login').style.display  = 'flex'
+    document.querySelector('#nova-senha').style.display = 'none'
+}
+
+document.querySelector('.nova-senha').onclick = () => {
+    document.querySelector('#cad').style.display    = 'none'  
+    document.querySelector('#login').style.display  = 'none'
+    document.querySelector('#nova-senha').style.display = 'flex'
+}
+
+document.querySelector('#link2').onclick = () => {
+    document.querySelector('#cad').style.display    = 'none'  
+    document.querySelector('#login').style.display  = 'flex'
+    document.querySelector('#nova-senha').style.display = 'none'
+}
+
