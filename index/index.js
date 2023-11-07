f46fb084f962106d0faf85b2859e0314ee287f52
santn11 <fernandonogueira.nogueira768@gmail.com>
2023-11-07 14:00:12 -0300

    07/11/2023

---
+++ index/index.js
@@ -0,0 +1,9 @@
+/* Exibir popup de configuração */
+document.querySelector('#btn-config').onclick = () => {
+    document.querySelector('#config').style.display= "flex"
+}
+
+/* Fechar popup de configuração */
+document.querySelector('#fechar-config').onclick = () => {
+    document.querySelector('#config').style.display= "none"
+}
